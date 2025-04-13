0c89e369a2995a2cc620233bda3dff781921e600
damien <courtier.damien@gmail.com>
2025-04-13 15:34:09 +0200

    edit config branch main

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: {
-    enabled: true,
+    enabled: false,
 
     timeline: {
-      enabled: true
+      enabled: false
     }
   },
   modules: ['@nuxt/ui', 'nuxt-lucide-icons','@nuxtjs/color-mode'],
